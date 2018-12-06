8478f41bb9ddfffac194a0d356be4c1fef5d9a33
user <user@users-MacBook-Pro.local>
2018-12-06 09:26:26 +0000

    Made a few small changes.

--- play.rb
+++ play.rb
@@ -4,32 +4,32 @@ require_relative('./player')
 
 
 
-p "Enter a word: "
+p "Enter a word/phrase:"
 word_to_guess = gets.chomp
 @hidden_word = HiddenWord.new(word_to_guess)
 system "clear"
 
-p "Enter your name: "
+p "Enter your name:"
 player_name = gets.chomp
 @new_player = Player.new(player_name, 6)
 guessed_letter = ""
 
 p "Hi #{@new_player.player_name.upcase}, you have #{@new_player.player_lives} lives remaining. "
 
-@game = Game.new(@new_player, @hidden_word,[])
+@game = Game.new(@new_player, @hidden_word,[" "])
 
 
 @hidden_word.replace_with_guessed_letters(@hidden_word.answer, @game.guessed_letters)
 
 while @new_player.player_lives > 0
-  p "Guess a letter: "
+  p "Guess a letter:"
   guessed_letter = gets.chomp
 
   if @game.compare_input_with_array(guessed_letter) == true
     p "You've already guessed that letter."
   else
     if @hidden_word.answer.include?(guessed_letter)
-      p "Correct!"
+      p "Correct! You still have #{@new_player.player_lives} lives remaining."
     else
       @new_player.remove_life
       p "Incorrect! #{@new_player.player_lives} lives remaining."
